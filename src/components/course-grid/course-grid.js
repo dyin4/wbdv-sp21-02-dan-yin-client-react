8c055cd661aea12471fbf7c4f4c75b38aff7c8f3
Dan Yin <danyin@Dans-MacBook-Pro.local>
2021-02-24 09:55:03 -0800

    add row-cols-lg-5 in card grid

--- src/components/course-grid/course-grid.js
+++ src/components/course-grid/course-grid.js
@@ -30,7 +30,7 @@ const CourseGrid = (props) =>
       </div>
 
       <div
-          className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-card">
+          className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-card">
         {props.courses.map((course, index) =>
             <CourseCard course={course}
                         key={course._id}
